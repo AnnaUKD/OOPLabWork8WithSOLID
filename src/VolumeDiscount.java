22d8f079dbb64708f19a62eaa852e74b52867c84
PogoAnnaUKDIPZ <anna.i.pohoreltseva@ukd.edu.ua>
2026-04-15 10:50:34 +0300

    Refactor VolumeDiscount class for clarity and correctness

--- src/VolumeDiscount.java
+++ src/VolumeDiscount.java
@@ -1,23 +1,20 @@
 public class VolumeDiscount implements DiscountStrategy {
-    private final double threshold;
-    private final double percent;
+    double threshold;
+    double percent;
 
     public VolumeDiscount(double threshold, double percent) {
         this.threshold = threshold;
         this.percent   = percent;
     }
 
-    @Override
     public double apply(double total) {
         if (total > threshold) {
-            return total * (1 - percent / 100);
-        } else {
-            return total;
+            return total - (total * percent / 100);
         }
+        return total;
     }
 
-    @Override
     public String description() {
-        return String.format("Знижка на обсяг (якщо > %.0f грн): -%.0f%%", threshold, percent);
+        return "Знижка на обсяг (якщо > " + threshold + " грн): -" + percent + "%";
     }
 }
